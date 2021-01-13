52d020aa39473db1b90bb9c2b50ffbcce343dceb
audreyfranco <74529775+audreyfranco@users.noreply.github.com>
2021-01-12 16:22:26 -0800

    current location button forecast update

--- src/weather.js
+++ src/weather.js
@@ -107,6 +107,8 @@ function searchLocation(position) {
   let units = "metric"
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeatherCondition);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentLocation(event) {
